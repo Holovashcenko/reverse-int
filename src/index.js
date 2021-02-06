2a42265130cd85e2d5a78d53358d2cfb93bf267f
Vladyslav Holovashchenko <holovashcenko@gmail.com>
2021-02-06 10:11:58 +0200

    fead: realized reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let arr = String(n).split("");
+    if (arr[0] == "-") return arr.slice(1).reverse().join("");
+    else return arr.reverse().join("");
+};
